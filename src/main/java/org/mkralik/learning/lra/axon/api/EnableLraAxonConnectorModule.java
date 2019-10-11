4a440904da57db0e3f6fd42176d7ae4c2b6b87aa
mkralik3 <mkralik@redhat.com>
2019-10-11 09:50:01 +0200

    Fixed dependency problem

--- src/main/java/org/mkralik/learning/lra/axon/api/EnableLraAxonConnectorModule.java
+++ src/main/java/org/mkralik/learning/lra/axon/api/EnableLraAxonConnectorModule.java
@@ -2,10 +2,11 @@ package org.mkralik.learning.lra.axon.api;
 
 import org.mkralik.learning.lra.axon.AfterStart;
 import org.mkralik.learning.lra.axon.AxonLraConfiguration;
-import org.mkralik.learning.lra.axon.interceptors.event.EventDispatchInterceptor;
-import org.mkralik.learning.lra.axon.interceptors.event.EventHandlerInterceptor;
 import org.mkralik.learning.lra.axon.interceptors.command.CommandDispatchInterceptor;
 import org.mkralik.learning.lra.axon.interceptors.command.CommandHandlerInterceptor;
+import org.mkralik.learning.lra.axon.interceptors.event.EventDispatchInterceptor;
+import org.mkralik.learning.lra.axon.interceptors.event.EventHandlerInterceptor;
+import org.mkralik.learning.lra.axon.rest.AxonLraEndpointsJaxRS;
 import org.mkralik.learning.lra.axon.rest.AxonLraEndpointsSpring;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -15,7 +16,14 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE})
 @Documented
-@Import({AxonLraConfiguration.class, CommandDispatchInterceptor.class, CommandHandlerInterceptor.class, EventDispatchInterceptor.class, EventHandlerInterceptor.class, AxonLraEndpointsSpring.class, AfterStart.class})
+@Import({AxonLraConfiguration.class,
+        CommandDispatchInterceptor.class,
+        CommandHandlerInterceptor.class,
+        EventDispatchInterceptor.class,
+        EventHandlerInterceptor.class,
+        AxonLraEndpointsSpring.class,
+        AxonLraEndpointsJaxRS.class,
+        AfterStart.class})
 @Configuration
 public @interface EnableLraAxonConnectorModule{
 }
